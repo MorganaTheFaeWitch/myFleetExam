29a1feaf7d5b3268ce50573e962a7c6671f9beca
Morgana The Witch <andrew.cox01110@gmail.com>
2026-03-19 13:04:46 +1100

    got it to skim data from the divisions in the json file

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -21,7 +21,7 @@ import {DateAdapter} from '@angular/material/core';
   styleUrl: './home.css',
 })
 
-export class Home implements OnInit, AfterViewInit {
+export class Home implements OnInit {
     private svr = serverConnector;  //initalising the server connector
     public data = this.svr.serverGetter(); //grabbing the data from the "server"
     public totalDistance = 0;
@@ -30,9 +30,7 @@ export class Home implements OnInit, AfterViewInit {
     ngOnInit() {
       // this.divisions = this.getDivisions(this.data);
       this.totalDistance = this.caculateKM("");
-    }
-    ngAfterViewInit() {
-      //console.log(this.divisions = this.getDivisions(this.data));
+      this.divisions = this.getDivisions(this.data);
     }
 
     // caculates the distance traveled for the fleet or division, it should be caculateKM(selected : string?) to allow for null inputs rather than always expecting a string
@@ -50,7 +48,7 @@ export class Home implements OnInit, AfterViewInit {
     // this was never actually used, the idea was to scrape the divisions from the fleet itself without requiring a discrete array for divisions,
     // however, i couldent determin if this was breaking or the dropdown was, so this was the first to be scrapped, I do believe it would work tho,
     // its just untested
-    private getDivisions = (json: any) => {
+    private getDivisions = (rawdta: any) => {
       // let lookup = {};
       // let dict = [];
       //   for (let i = 0; i < json.length; i++) {
@@ -62,12 +60,19 @@ export class Home implements OnInit, AfterViewInit {
       //   }
       // return divisions;
       //console.log(json.filter((obj: { id: any; }, index: any, self: any[]) => index === self.findIndex((t) => t.id === obj.id)));
-      // this was found on this GeeksForGeeks article, https://www.geeksforgeeks.org/javascript/how-to-get-distinct-values-from-an-array-of-objects-in-javascript/
-      let rawdivs = json.filter((obj: { division: any; }, index: any, self: { division: any; }[]) => index === self.findIndex((t: { division: any; }) => t.division === obj.division));
-      let divisions = [];
-      for (let i = 0; i < rawdivs.length; i++) {
-        divisions.push(rawdivs[i].division);
+      try{
+        let ListOfDivs = rawdta.fleet;
+        // this was found on this GeeksForGeeks article, https://www.geeksforgeeks.org/javascript/how-to-get-distinct-values-from-an-array-of-objects-in-javascript/
+        let rawdivs = ListOfDivs.filter((obj: { division: any; }, index: any, self: { division: any; }[]) => index === self.findIndex((t: { division: any; }) => t.division === obj.division));
+        let divisions = [];
+        for (let i = 0; i < rawdivs.length; i++) {
+          divisions.push(rawdivs[i].division);
+        }
+        return divisions;
       }
-      return divisions;
+      catch (e) {
+        console.log(e);
+      }
+      return [];
     }
 }
